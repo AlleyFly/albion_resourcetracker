50273697718ec10d64e93409f7ce724142c59aa4
Jan <ggnoettgen@gmx.net>
2019-04-26 21:08:34 +0200

    somewhat fresher look

--- gui_list.py
+++ gui_list.py
@@ -9,6 +9,8 @@ mats = {
 	4: "cloth"
 }
 
+tiers = ["T2", "T3", "T4", "T5", "T6", "T7", "T8"]
+
 class GUI:
 
 		
@@ -37,35 +39,13 @@ class GUI:
 		clear.pack(side=LEFT)
 		
 		#buttonTable labels
-		labelt0 = Label(self.intab, text="Tier")
-		labelt2 = Label(self.intab, text="T2")
-		labelt3 = Label(self.intab, text="T3")
-		labelt4 = Label(self.intab, text="T4")
-		labelt5 = Label(self.intab, text="T5")
-		labelt6 = Label(self.intab, text="T6")
-		labelt7 = Label(self.intab, text="T7")
-		labelt8 = Label(self.intab, text="T8")
-		
-		labelt0.grid(row=0, column=0)
-		labelt2.grid(row=0, column=1)
-		labelt3.grid(row=0, column=2)
-		labelt4.grid(row=0, column=3)
-		labelt5.grid(row=0, column=4)
-		labelt6.grid(row=0, column=5)
-		labelt7.grid(row=0, column=6)
-		labelt8.grid(row=0, column=7)
-		
-		labelWood = Label(self.intab, text="Wood")
-		labelBrick = Label(self.intab, text="Brick")
-		labelLeather = Label(self.intab, text="Leather")
-		labelMetal = Label(self.intab, text="Metal")
-		labelCloth = Label(self.intab, text="Cloth")
-
-		labelWood.grid(row=1, sticky=NSEW)
-		labelBrick.grid(row=2, sticky=NSEW)
-		labelLeather.grid(row=3, sticky=NSEW)
-		labelMetal.grid(row=4, sticky=NSEW)
-		labelCloth.grid(row=5, sticky=NSEW)
+		for i in range(7):
+			label = Label(self.intab, text=tiers[i])
+			label.grid(row=0, column=i+1)
+			
+		for i in range(5):
+			label = Label(self.intab, text=mats[i].capitalize())
+			label.grid(row=i+1, stick=W)
 		
 		for i in range(5):
 			self.buttons.append([])
